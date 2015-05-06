ed94fdb52637519e04189075776be858e7938104
Brendan McDonald <brendan@brendandmcdonald.com>
2015-05-06 14:41:24 +0100

    finished the follow-along messages

--- Dice Incomplete/RollViewController.swift
+++ Dice Incomplete/RollViewController.swift
@@ -19,8 +19,34 @@ class RollViewController: UIViewController {
         // Return a more convenient Int, initialized with the random value
         return Int(randomValue)
     }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        let controller = segue.destinationViewController as! DiceViewController
+        
+        controller.firstValue = self.randomDiceValue()
+        controller.secondValue = self.randomDiceValue()
+        /* BETTER FOUNDATION FOR MULTTIPLE SEGUES
+        When my apps get more complex and there is more than one potential segue, using an if statement will help the app determine which segue (and which necessary data) to send and where.
+        
+        if segue.identifier == "rollDice" {
+        
+        let controller = segue.destinationViewController as!
+        DiceViewController
+        
+        controller.firstValue = self.randomDiceValue()
+        controller.secondValue = self.randomDiceValue()
+        }
+        */
+        
+    }
+    
 
     @IBAction func rollTheDice() {
+        
+        //performSegueWithIdentifier("rollDice", sender: self)
+        
+        
+        /* PROGRAMMATIC SEGUE
         var controller: DiceViewController
         
         controller = self.storyboard?.instantiateViewControllerWithIdentifier("DiceViewController") as! DiceViewController
@@ -29,6 +55,7 @@ class RollViewController: UIViewController {
         controller.secondValue = self.randomDiceValue()
         
         self.presentViewController(controller, animated: true, completion: nil)
+        */
     }
     
     
